4f6dd3b0a25bdd307ddc5a4a6143d49e6a98be18
StephanRaab <sarcartist@gmail.com>
2016-10-23 17:41:52 -0400

    trying out some more faker techniques

--- introToNpm/myshop/listproducts.js
+++ introToNpm/myshop/listproducts.js
@@ -3,6 +3,11 @@ var faker= require('faker');
 var randomProduct = faker.commerce.product();
 var price = faker.commerce.price();
 
+console.log("==================");
+console.log("WELCOME TO MY SHOP");
+console.log("==================");
+
 for(var i = 0; i < 10; i++) {
 	console.log(faker.fake('{{commerce.product}}, {{commerce.price}}'));
 }
+
