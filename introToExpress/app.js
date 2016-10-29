2bafd902eb1dbda8afb7b31bfff67d16685029e4
StephanRaab <sarcartist@gmail.com>
2016-10-29 19:31:02 -0400

    node route orders and route matching

--- introToExpress/app.js
+++ introToExpress/app.js
@@ -10,14 +10,31 @@ app.get("/", function(request, response) {
 // '/bye' --> 'goodbye'
 app.get("/bye", function(req, res) {
 	res.send("Goodbye!!");
-})
+});
 
 // '/dog' --> 'woof'
 app.get("/dog", function(req, res) {
 	res.send("WOOF!!!");
-})
+});
+
+//subreddit
+app.get("/r/:subredditName", function(req, res){
+  var subreddit = req.params.subredditName;	
+	res.send("welcome to the "+ subreddit + " subreddit");
+});
+
+//subreddit page with comments
+app.get("/r/:subreddit/comments/:id/:title", function(req, res) {
+	console.log(req.params);
+	res.send("welcome to the subreddit comments page!");
+});
+
+//wildcard
+app.get("/*", function (req, res) {
+	res.send("You are a star!");
+});
 
 // tell express to listen for requests (start server)
 app.listen(3000, function() {
 	console.log("app running on port:3000");
-})
+});
