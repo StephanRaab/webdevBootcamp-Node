650a0c09036b990872703d08474f0759f123516a
StephanRaab <sarcartist@gmail.com>
2016-10-30 14:29:53 -0400

    equal to double equal sign fix

---
+++ introToExpress/firstExpressApp/app.js
@@ -0,0 +1,51 @@
+var express = require ('express');
+var app = express();
+
+//visiting "/" should print "Hi there, welcom to my assignment"
+app.get("/", function(req, res){
+	res.send('Hi there, welcome to my assignment');
+});
+
+//visiting "/speak/pig" should print "the pig says 'Oink'"
+//visiting "/speak/cow" should print "the cow says 'Moo'"
+//visiting "/speak/dog" should print "the dog says 'Woof'"
+app.get("/speak/:animal", function(req, res){
+	var animal = req.params.animal.toLowerCase();
+	var animalSound;
+	var sounds = {
+		dog: "woof",
+		cat: "'i hate you human'",
+		cow: "moo",
+		pig: "oink",
+		fish: "..."
+	}
+ 
+ animalSound = sounds[animal];
+
+	res.send("The "+ animal + " says " + animalSound + ".");
+})
+
+//visiting "/repeat/hello/3" should print out "hello hello hello"
+//visiting "/repeat/hello/5" should print out "hello hello hello hello hello"
+//visiting "/repeat/blah/2" should print out "blah blah"
+app.get("/repeat/:repeatWord/:repeatAmount", function(req, res){
+	function runCommand() {
+		var repeatWord = req.params.repeatWord;
+		var repeatAmount = parseInt(req.params.repeatAmount);
+
+		for (var i = 0; i < repeatAmount.length; i++) {
+			console.log(repeatWord);
+		}
+	}
+	res.send(runCommand());
+});
+
+//if any other route print:
+//"Sorry, page not found...What are you doing with your life? "
+app.get("*", function(req, res){
+	res.send("Sorry, page not found...What are you doing with your life?");
+});
+
+app.listen(3000, function(){
+	console.log("App running on localhost:3000");
+});
