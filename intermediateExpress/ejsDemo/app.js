e05855a4118bd6958489c19fd8b301c64d76c2a7
StephanRaab <sarcartist@gmail.com>
2016-11-05 12:22:46 -0400

    express styles and partials to dry up code, public assets

--- intermediateExpress/ejsDemo/app.js
+++ intermediateExpress/ejsDemo/app.js
@@ -1,16 +1,22 @@
 var express = require ('express');
 var app = express();
- 
+
+//this sets the public directory up css won't work otherwise
+app.use(express.static('public'));
+
+//view engine lets you leave off the .ejs
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
 	//res.render("name of file goes here")
-	res.render("home.ejs");
+	res.render("home");
 })
 
 app.get("/thingsilove/:thing", function(req, res){
 	var thing = req.params.thing;
 
 	//thingVar is the variable inside of the love.ejs file
-	res.render("love.ejs", {thingVar: thing});
+	res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -20,7 +26,18 @@ app.get("/posts", function(req, res) {
 		{title: "Final post", author: "revelation"}
 	];
 
-	res.render("posts.ejs", {posts: posts});
+	res.render("posts", {posts: posts});
+})
+
+app.get("/moreposts", function(req, res) {
+	var posts = [
+		{title: "Intro",author: "b3l914n"},
+		{title: "Space", author: "Michael Jordan"},
+		{title: "Final post", author: "revelation"},
+		{title: "Allergy Medicine", author: "now"}
+	];
+
+	res.render("moreposts", {posts: posts});
 })
 
 app.listen(3000, function(){
